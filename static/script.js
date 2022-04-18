d167378d943a50ea634a6f7735470a6c45057ba5
lucianojunnior17 <engluciano.junior@gmail.com>
2022-04-18 18:11:58 -0300

    Habilidades

--- static/script.js
+++ static/script.js
@@ -9,5 +9,15 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("nav-menu-active");
 
-
 });
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  })
+})
